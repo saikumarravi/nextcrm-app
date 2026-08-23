1a2227398be1ffbad4cb5cd22cde10e7330bfc18
saikumarravi <sai@techsaasworks.com>
2026-08-23 06:13:17 +0530

    Set default values for Inngest ID and name

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,8 +1,8 @@
 import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
-  id: process.env.INNGEST_ID as string,
-  name: process.env.INNGEST_APP_NAME as string,
+  id: process.env.INNGEST_ID || "nextcrm",
+  name: process.env.INNGEST_APP_NAME || "NextCRM",
   eventKey: process.env.INNGEST_EVENT_KEY,
   signingKey: process.env.INNGEST_SIGNING_KEY,
 });
